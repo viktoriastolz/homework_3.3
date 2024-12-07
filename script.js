17e6462e1b2bce62babcf3566ca3219aae8808f1
Viktoria Stolz <88290448+viktoriastolz@users.noreply.github.com>
2024-12-07 14:32:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+// "use strict";
+
+// const number = prompt("Enter a five-digit number:");
+
+// console.log(number.split('').join(' '));
 "use strict";
 
-const number = prompt("Enter a five-digit number:");
+function processFiveDigitNumber() {
+    const number = prompt("Enter a five-digit number:");
 
-console.log(number.split('').join(' '));
+    if (!number || number.length !== 5 || !/^\d{5}$/.test(number)) {
+        alert("Invalid input! Please enter a valid five-digit number.");
+        return;
+    }
+    const formattedNumber = number.split('').join(' ');
+    alert(`Formatted number: ${formattedNumber}`);
+}
+
+processFiveDigitNumber();
