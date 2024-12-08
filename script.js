54bf03a55188801186f4d7dcdf9bc0ecc3d959c4
Viktoria Stolz <88290448+viktoriastolz@users.noreply.github.com>
2024-12-09 00:38:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,9 @@ if (fiveDigitNumberInput === null) {
     alert("Ok, bye");
 } else {
     if (fiveDigitNumberInput.trim() === "" || isNaN(fiveDigitNumberInput) || fiveDigitNumberInput.length !== 5) {
-        alert("Error: invalid number");
+        alert("Error: Invalid number");
     } else {
-        const num = Math.abs(+fiveDigitNumberInput); 
-        if (String(num).length === 5) {
-            alert(`Valid five-digit number: ${num}`);
-        } else {
-            alert("Number is not a five-digit number");
-        }
+        const digits = fiveDigitNumberInput.split('').join(' ');
+        alert(`The number split into digits: ${digits}`);
     }
 }
